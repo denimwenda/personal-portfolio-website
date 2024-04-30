810ea082b612bb6665fd02163ac60d429ae0f981
denimwenda <denimwendwa@gmail.com>
2024-04-30 20:54:54 +0300

    changes

--- main.js
+++ main.js
@@ -34,4 +34,52 @@ let changeText = ()=>{
 };
 
 changeText();
-setInterval(changeText, 3000)
+setInterval(changeText,3000)
+
+
+// circle skill ////////////////////////////////////
+const circles = document.querySelectorAll('.circle');
+circles.forEach(elem=>{
+    var dots = elem.getAttribute("data-dots");
+    var marked = elem.getAttribute("data-percent");
+    var percent = Math.floor(dots*marked/100);
+    var points = "";
+    var rotate = 360 / dots;
+
+
+    for(let i = 0 ; i < dots ; i++){
+        points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`
+    }
+    elem.innerHTML = points;
+
+    const pointsMarked = elem.querySelectorAll('.points');
+    for(let i = 0; i<percent ; i++){
+        pointsMarked[i].classList.add('marked')
+    }
+})
+
+
+// mix it up portfolio section
+var mixer = mixitup('.portfolio-gallery');
+
+
+
+// active menu ////////////////////////////
+let menuLi = document.querySelectorAll('header ul li a');
+let section = document.querySelectorAll('section');
+
+function activeMenu(){
+    let len = section.length;
+    while(--len && window.scrollY + 97 < section[len].offsetTop){}
+    menuLi[len].classList.add("active")
+}
+
+activeMenu();
+window.addEventListener("scroll",activeMenu)
+
+
+// sticky navbar /////////////////////////////////
+const header = document.querySelector("header");
+window.addEventListener("scroll",function(){
+    header.classList.toggle("sticky",this.window.scrollY > 50)
+})
